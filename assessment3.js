20a523f336430c527d64dc1024aaa2500326e51f
TXMack713 <mackattack713@gmail.com>
2021-06-22 16:13:59 -0500

    1 file modified, 8 added, 1 deleted

---
+++ assessment3.js
@@ -0,0 +1,14 @@
+let quote = 'I dO nOT lIke gREen eGgS anD HAM';
+
+// Add your code here
+
+// Don't edit the code below here!
+
+section.innerHTML = ' ';
+let para1 = document.createElement('p');
+para1.textContent = fixedQuote;
+let para2 = document.createElement('p');
+para2.textContent = finalQuote;
+
+section.appendChild(para1);
+section.appendChild(para2);
